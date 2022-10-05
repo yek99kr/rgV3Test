607015b9b6278bdc5c337c0869bb111c0a52ba48
Rachel Kim <yek99kr@gmal.com>
2022-10-05 10:43:11 -0400

    space

--- components/Shop/MiniCart.js
+++ components/Shop/MiniCart.js
@@ -286,7 +286,7 @@ export default function MiniCart({ cart }) {
                     <div className="flex-1 px-4 py-6 overflow-x-hidden overflow-y-auto sm:px-6">
                       <div className="mt-0 flex-1 lg:block w-[97vw] sm:w-[520px] md:w-[570px] lg:w-[400px]">
                         <div className="flow-root">
-                          <div className="flex place-content-end mr-3 h-[3vw] 2xs:h-7 xs:h-8 sm:h-8">
+                          <div className="hidden lg:flex place-content-end mr-3 h-8">
                             <button
                               ref={cancelButtonRef}
                               type="button"
@@ -305,7 +305,7 @@ export default function MiniCart({ cart }) {
                               {cart.map((product) => (
                                 <li
                                   key={product.id + Math.random()}
-                                  className="relative flex py-4 bg-white/60 backdrop-blur-md rounded pl-5 pr-5 m-3 mt-5"
+                                  className="relative flex py-4 bg-white/60 backdrop-blur-md rounded pl-5 pr-5 m-3 mt-[10vw] xs:mt-[3.5rem] sm:mt-[3.5rem] lg:mt-5"
                                 >
                                   <div
                                     className={`top-0 left-0 right-0 z-1000 w-full h-full absolute ${
@@ -414,7 +414,7 @@ export default function MiniCart({ cart }) {
                             </ul>
                           ) : (
                             <div
-                              className="thumbcursor rounded m-3 p-3 bg-white/60 backdrop-blur-md text-[2.9vw] sm:text-sm md:text-base"
+                              className="thumbcursor rounded m-3 mt-[10vw] xs:mt-[3rem] sm:mt-[3rem] lg:mt-5 p-3 bg-white/60 backdrop-blur-md text-[2.9vw] sm:text-sm md:text-base"
                               onClick={() => setCartOpen(false)}
                             >
                               <p>Nothing in your cart!</p>
