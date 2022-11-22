ae0a7f7d7aa3bfafa4ff74f65c25e07d1cd53fe9
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2022-11-22 09:35:35 -0500

    d

--- pages/api/subscribeUser.js
+++ pages/api/subscribeUser.js
@@ -1,44 +1,44 @@
-import fetch from "isomorphic-unfetch";
+// import fetch from "isomorphic-unfetch";
 
-export default async (req, res) => {
-  const { email } = req.body;
+// export default async (req, res) => {
+//   const { email } = req.body;
 
-  console.log({ email });
+//   console.log({ email });
 
-  if (!email) {
-    return res.status(400).json({ error: "Email is required" });
-  }
+//   if (!email) {
+//     return res.status(400).json({ error: "Email is required" });
+//   }
 
-  try {
-    const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
-    const API_KEY = process.env.MAILCHIMP_API_KEY;
-    const DATACENTER = process.env.MAILCHIMP_API_SERVER;
-    const data = {
-      email_address: email,
-      status: "subscribed",
-    };
+//   try {
+//     const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
+//     const API_KEY = process.env.MAILCHIMP_API_KEY;
+//     const DATACENTER = process.env.MAILCHIMP_API_SERVER;
+//     const data = {
+//       email_address: email,
+//       status: "subscribed",
+//     };
 
-    const response = await fetch(
-      `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
+//     const response = await fetch(
+//       `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`,
 
-      {
-        body: JSON.stringify(data),
-        headers: {
-          Authorization: `apikey ${API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      }
-    );
+//       {
+//         body: JSON.stringify(data),
+//         headers: {
+//           Authorization: `apikey ${API_KEY}`,
+//           "Content-Type": "application/json",
+//         },
+//         method: "POST",
+//       }
+//     );
 
-    if (response.status >= 400) {
-      return res.status(400).json({
-        error: `There was an error subscribing to the newsletter.`,
-      });
-    }
+//     if (response.status >= 400) {
+//       return res.status(400).json({
+//         error: `There was an error subscribing to the newsletter.`,
+//       });
+//     }
 
-    return res.status(201).json({ error: "" });
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
-  }
-};
+//     return res.status(201).json({ error: "" });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message || error.toString() });
+//   }
+// };
