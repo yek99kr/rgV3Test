3647005bfbb2a423f947ddc73a789071e20f73d0
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2022-12-01 14:11:36 -0500

    gg

--- pages/shop.js
+++ pages/shop.js
@@ -7,10 +7,19 @@ const store = ({ router, products }) => {
     <>
       <motion.div
         key={router.route}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.5 }}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        transition={{ delay: 0.3, default: { duration: 0.7 } }}
+        variants={{
+          pageInitial: { opacity: 0 },
+          pageAnimate: {
+            opacity: 1,
+          },
+          pageExit: {
+            opacity: 0,
+          },
+        }}
         className="overflow-hidden"
       >
         <ProductList products={products} />
