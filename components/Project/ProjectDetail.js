ee9515e5edafd40e85f154836acd6fabe1da6131
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2022-12-08 15:06:11 -0500

    bg

--- components/Project/ProjectDetail.js
+++ components/Project/ProjectDetail.js
@@ -41,14 +41,14 @@ const ProjectDetail = ({ project, projects }) => {
         </div> */}
 
         <div className="absolute bg-red text-center w-[90vw] h-[11vw] lg:h-[5vw] top-[22vw] md:top-[23vw] lg:top-[21vw] xl:top-[23vw] z-[100]">
-          <h1 className="text-[5.7vw] sm:text-[40px] lg:text-[65px]">
+          <h1 className="text-[5.7vw] sm:text-[40px] lg:text-[50px] 2xl:text-[55px]">
             <span className="text-white bg-black p-[0.3vw] md:p-0.5 pl-3 pr-3 font-bold">
               {project.data.client}
             </span>
           </h1>
 
-          <p className="title relative text-[3.1vw] sm:text-[22px] lg:text-[28px] mb-[2vw] md:mb-[30px] font-mono">
-            <span className="text-white bg-black p-[0.3vw] md:p-0.5 pl-2 pr-2">
+          <p className="title relative text-[3.1vw] sm:text-[22px] lg:text-[24px] xl:text-[26px] mb-[2vw] md:mb-[30px] font-mono">
+            <span className="text-white bg-black p-[0.2vw] md:p-0.5 pl-2 pr-2">
               {project.data.title}
             </span>
           </p>
